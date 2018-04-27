668c8109d3a5589145aa1ab866ac07df27488154
soul <relsoul@outlook.com>
2018-04-27 14:10:34 +0800

    update env

--- conf/conn.js
+++ conf/conn.js
@@ -1,5 +1,14 @@
-const conf = require('../conf/conf');
+let conf = require('../conf/conf');
 const Sequelize = require('sequelize');
+
+
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+if (env === 'test') {
+    conf = require('./conf/conf-ci.js');
+}
+
+console.log('conn env', env);
 
 const sequelize = new Sequelize(conf.mysql.database, conf.mysql.user, conf.mysql.password, {
     host: conf.mysql.host,
